6b0e6c99e778a4c8fe5b5841861fff47fbf00f68
Nausheen Faiyaz <logme2nausheen@gmail.com>
2026-02-22 16:44:53 +0530

    feat: add custom mymap polyfill implementation

--- polyfill.js
+++ polyfill.js
@@ -2,9 +2,8 @@
 
 const arr = [1, 2, 3, 4, 5, 6];
 
-// Real Signature - No return, function input param, value, index
+// Signature .forEach - No return, function input param, value, index
 // calls my fn for every value
-
 
 if (!Array.prototype.myForEach) {
   Array.prototype.myForEach = function (callback) {
@@ -14,9 +13,26 @@ if (!Array.prototype.myForEach) {
   };
 }
 
-
-const ret= arr.myForEach(function (value, index) {
+const ret = arr.myForEach((value, index) => {
   console.log(`My ForEach Value at Index ${index} is ${value}`);
 });
 
-// console.log(`Ret is`, ret);
+console.log(`Ret is`, ret); // undefined
+
+// Signature .map - Return new array, each element iterate
+
+if (!Array.prototype.myMap) {
+  Array.prototype.myMap = function (callback) {
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const value = callback(this[i], i);
+      result.push(value);
+    }
+
+    return result;
+  };
+}
+
+const newArr = arr.myMap((e) => e * 3);
+console.log(newArr);
